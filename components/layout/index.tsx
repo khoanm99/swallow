56cc32f58e9d90346aa90d84b2a4404dcd5d0db5
khoa <khoa@vietswiss.com>
2022-10-03 13:07:54 +0700

    init project to ts

---
+++ components/layout/index.tsx
@@ -0,0 +1,15 @@
+import styled from 'styled-components'
+
+const Layout = ({ children} : any) => {
+  return (
+    <LayoutContainer>
+
+    </LayoutContainer>
+  )
+}
+
+export default Layout
+
+const LayoutContainer = styled.div`
+  padding: 20px;
+`
